181054e3bb757dc4da50321e31582a2fb258690b
LeonGZZ <88094776+LeonGZZ@users.noreply.github.com>
2021-07-28 20:52:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Hey yo em bé yêu!',
+    introDesc: `Chúc mừng sinh nhật công chúa tuổi 19 hay ăn mau lớn nhé`,
+    btnIntro: 'Tao biết rồi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    desc: 'Ngày sinh nhật của em hôm nay có vui không?',
+    btnYes: 'Có ạ, vui lắm ạ',
+    btnNo: 'Đéo nhé đm thằng chó Thắng',
+    question:'Nhân dịp sinh nhật ngày hôm nay, em có muốn được tặng món quà gì không?',
+    btnReply: 'Xác nhận luôn!',
+    reply: 'Đang dịch thế này tặng quà online đi, 500k bank nóng đi chồng iu',
+    mess: 'Chúc mừng bạn đã được tặng 500k vào tài khoản',
+    messDesc: 'Hệ thống sẽ xử lí và thông báo trên điện thoại của bạn khi hoàn tất',
+    btnAccept: 'Được thực sự!',
+    messLink: 'https://drive.google.com/file/d/1CwuUktQ8h8zrLUNV3Q7yo4ldFvqVCADd/view?usp=sharing' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
